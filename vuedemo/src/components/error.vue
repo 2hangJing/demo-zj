b289f2e7ba9bbda31c51e186b1e51e7b072d8ee4
zj <15901233752@163.com>
2017-04-23 13:57:34 +0800

    home 13:57

--- vuedemo/src/components/error.vue
+++ vuedemo/src/components/error.vue
@@ -3,7 +3,7 @@
 		<h1 style="text-align: center;">遥远的404！</h1>
 		<div style="width: 150px;margin: 50px auto;">
 			<button v-on:click="jump(0)" class="btn btn-primary">首页</button>
-			<button v-on:click="jump(0)" class="btn btn-primary">第一页</button>
+			<button v-on:click="jump(1)" class="btn btn-primary">第一页</button>
 		</div>
 	</div>
 </template>
